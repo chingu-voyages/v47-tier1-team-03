907127a2b40109471d57e440f37e845a94550837
Morais-C <morais.carlos@hotmail.com>
2024-01-17 18:57:23 +0000

    Proof of Concept 001

--- POC_001/index.js
+++ POC_001/index.js
@@ -1,6 +1,5 @@
-import { calendar } from "./data.js";
 
-// (C1) DATA ARRAY
+// DEMO DATA ARRAY
 var data = [
     ["Routine Activities", "M", "T", "W", "T", "F", "Sa", "S", "M", "T", "W", "T", "F", "Sa", "S"],
     ["Projects", "1", "2", "3", "4", "5", "6", "7" , "8", "9", "10", "11", "12", "13", "14"],
@@ -8,11 +7,11 @@ var data = [
     ["Monday", '<input type="checkbox"/>', '<input type="checkbox"/>', '<input type="checkbox"/>',  '<input type="checkbox"/>', '<input type="checkbox"/>',  '<input type="checkbox"/>', '<input type="checkbox"/>', '<input type="checkbox"/>', '<input type="checkbox"/>',  '<input type="checkbox"/>', '<input type="checkbox"/>',  '<input type="checkbox"/>', '<input type="checkbox"/>', '<input type="checkbox"/>'],
 ];
 
-// (C2) SET NUMBER OF COLUMNS
+// SET NUMBER OF COLUMNS
 var grid = document.getElementById("daily_checklist");
 grid.style.cssText = `grid-template-columns:repeat(${data[0].length}, minmax(0, 1fr))`;
 
-// (C3) FIRST ROW - HEADER
+// FIRST ROW - HEADER
 for (let i of data[0]) {
     let cell = document.createElement("div");
     cell.innerHTML = i;
@@ -20,7 +19,7 @@ for (let i of data[0]) {
     grid.appendChild(cell);
 }
 
-// (C3) Second ROW - Sub-HEADER
+// Second ROW - SUB-HEADER
 for (let i of data[1]) {
     let cell = document.createElement("div");
     cell.innerHTML = i;
@@ -28,7 +27,7 @@ for (let i of data[1]) {
     grid.appendChild(cell);
 }
 
-// (C4) FOLLOWING ROWS - CELLS
+// FOLLOWING ROWS - CELLS
 for (let i = 2; i < data.length; i++) {
     for (let j of data[i]) {
         let cell = document.createElement("div");
@@ -37,16 +36,3 @@ for (let i = 2; i < data.length; i++) {
         grid.appendChild(cell);
     }
 }
-
-// let demoHTML = ``;
-
-// calendar.forEach((item) => {
-//     demoHTML += `
-//         <div class="category">${item.categoryName}</div>
-//         ${item.activityTypes.forEach((activity) => {
-            
-//     })}
-//         <div class="acti">${item.categoryName}</div>
-//     `
-// })
-
