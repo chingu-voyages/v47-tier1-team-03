4b59333feeefe20fbdf6bdb06198c78cc1c3a304
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-01-30 13:28:17 +0000

    just added a little bit of CSS for practice

--- index.js
+++ index.js
@@ -12,60 +12,58 @@ renderCalendar();
 renderTasks();
 
 function renderCalendar() {
+  let datesHtml = "";
+  let daysHtml = "";
 
-    let datesHtml = '';
-    let daysHtml = '';
+  const daysEl = document.getElementById("weekdays");
+  const datesEl = document.getElementById("month-dates");
 
-    const daysEl = document.getElementById('weekdays')
-    const datesEl = document.getElementById('month-dates')
+  for (let i = 1; i <= daysInMonth; i++) {
+    let currentDate = new Date(year, month, i);
 
-    for (let i = 1; i <= daysInMonth; i++) {
-        let currentDate = new Date(year, month, i);
-
-        let currentDayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(currentDate);
-        if (currentDayOfWeek === 'Sat') {
-            currentDayOfWeek = 'Sa'
-        } else {
-            currentDayOfWeek = currentDayOfWeek[0];
-        }
-
-        daysHtml += `<p class=''>${currentDayOfWeek}</p>`;
-        datesHtml += `<p class=''>${i}</p>`;
+    let currentDayOfWeek = new Intl.DateTimeFormat("en-US", {
+      weekday: "short",
+    }).format(currentDate);
+    if (currentDayOfWeek === "Sat") {
+      currentDayOfWeek = "Sa";
+    } else {
+      currentDayOfWeek = currentDayOfWeek[0];
     }
 
-    daysEl.innerHTML = daysHtml;
-    datesEl.innerHTML = datesHtml;
+    daysHtml += `<p class=''>${currentDayOfWeek}</p>`;
+    datesHtml += `<p class=''>${i}</p>`;
+  }
+
+  daysEl.innerHTML = daysHtml;
+  datesEl.innerHTML = datesHtml;
 }
 
 function renderTasks() {
-    // I used Carlos POC_002 adding backticks (``) and $ signs
-    // to attribute each element a class to each item.
+  // I used Carlos POC_002 adding backticks (``) and $ signs
+  // to attribute each element a class to each item.
 
-    calendar.forEach( category => {
+  calendar.forEach((category) => {
+    text += `
+    <p class="categories">${category.categoryName}</p>`;
+
+    category.activityTypes.forEach((activity) => {
+      text += `
+    <p class="activities">${activity.activityName}</p>`;
+
+      activity.Tasks.forEach((task) => {
+        text += `
+        <p class="task-days task-style">${task.days}</p>`;
 
         text += `
-    <p class="categories">${category.categoryName}</p>`
+        <p class="task-name task-style">${task.taskName}</p>`;
 
-        category.activityTypes.forEach( activity => {
-
-            text += `
-    <p class="activities">${activity.activityName}</p>`
-
-            activity.Tasks.forEach( task => {
-
-                text += `
-        <p class="task-days">${task.days}</p>`
-
-                text += `
-        <p class="task-name">${task.taskName}</p>`
-
-                for (let i=1; i<= daysInMonth; i++) {
-                    text += `
-                    <input type="checkbox">`
-                }
-            });
-        });
+        for (let i = 1; i <= daysInMonth; i++) {
+          text += `
+                    <input type="checkbox">`;
+        }
+      });
     });
+  });
 
-    document.getElementById("test_div").innerHTML = text;
+  document.getElementById("test_div").innerHTML = text;
 }
