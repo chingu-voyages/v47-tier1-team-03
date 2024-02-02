c7727a04053c24f5aa4198aba5744ac16d41820c
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-02-02 19:51:34 +0100

    Rendering of current day bar

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { calendar } from "./data.js";
 
 let text = ``;
-
+// Date variables
 const now = new Date();
 const year = now.getFullYear();
 const month = now.getMonth();
 const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-
+// calling functions
 renderCalendar();
 
 renderTasks();
@@ -15,92 +15,102 @@ renderTasks();
 adaptCheckboxClass();
 
 function renderCalendar() {
-  let datesHtml = "";
-  let daysHtml = "";
+    let datesHtml = "";
+    let daysHtml = "";
 
-  const daysEl = document.getElementById("weekdays");
-  const datesEl = document.getElementById("month-dates");
+    const daysEl = document.getElementById("weekdays");
+    const datesEl = document.getElementById("month-dates");
 
-  for (let i = 1; i <= daysInMonth; i++) {
-    let currentDate = new Date(year, month, i);
+    for (let i = 1; i <= daysInMonth; i++) {
+        let currentDate = new Date(year, month, i);
 
-    let currentDayOfWeek = new Intl.DateTimeFormat("en-US", {
-      weekday: "short",
-    }).format(currentDate);
-    if (currentDayOfWeek === "Sat") {
-      currentDayOfWeek = "Sa";
-    } else {
-      currentDayOfWeek = currentDayOfWeek[0];
+        let currentDayOfWeek = new Intl.DateTimeFormat("en-US", {
+            weekday: "short",
+        }).format(currentDate);
+
+        if (currentDayOfWeek === "Sat") {
+            currentDayOfWeek = "Sa";
+        } else {
+            currentDayOfWeek = currentDayOfWeek[0];
+        }
+        if (i === now.getDate()) {
+            daysHtml += `
+              <div class='todays-date'>
+                <p>${currentDayOfWeek}</p>
+              </div>`;
+            datesHtml += `
+            <div class='todays-date'>
+              <p>${i}</p>
+            </div>`;
+        } else {
+            daysHtml += `
+            <div>
+              <p class=''>${currentDayOfWeek}</p>
+            </div>`;
+            datesHtml += `
+            <div>
+              <p class=''>${i}</p>
+            </div>`
+        }
     }
-    if(i === now.getDate()){
-      daysHtml += `<span class='red'>${currentDayOfWeek}</span>`;
-      datesHtml += `<span class='red'>${i}</span>`;
-    }
-      else {daysHtml += `<p class=''>${currentDayOfWeek}</p>`;
-      datesHtml += `<p class=''>${i}</p>`}
-    
-  }
-
-  daysEl.innerHTML = daysHtml;
-  datesEl.innerHTML = datesHtml;
+    daysEl.innerHTML = daysHtml;
+    datesEl.innerHTML = datesHtml;
 }
-
+// Rendering JSON and checkboxes
 function renderTasks() {
-  // I used Carlos POC_002 adding backticks (``) and $ signs
-  // to attribute each element a class to each item.
 
-  calendar.forEach((category) => {
-    text += `
-    <p class="categories">${category.categoryName}</p>`;
-
-    category.activityTypes.forEach((activity) => {
-      text += `
-    <p class="activities">${activity.activityName}</p>`;
-
-      activity.Tasks.forEach((task) => {
+    calendar.forEach((category) => {
         text += `
-        <p class="task-days task-style">${task.days}</p>`;
+  <p class="categories">${category.categoryName}</p>`;
 
-        text += `
-        <p class="task-name task-style">${task.taskName}</p>`;
-
-        for (let i = 1; i <= daysInMonth; i++) {
-            let weekday = convertDayToWeekDay(i);
+        category.activityTypes.forEach((activity) => {
             text += `
-            <input 
-            type="checkbox"
-            data-day="${i}" 
-            data-weekday="${weekday}" 
-            data-assigned-day="${task.days}"
-            <span class="checkbox"></span>`
-        }
-      });
+      <p class="activities">${activity.activityName}</p>`;
+
+            activity.Tasks.forEach((task) => {
+                text += `
+          <p class="task-days task-style">${task.days}</p>`;
+
+                text += `
+            <p class="task-name task-style">${task.taskName}</p>`;
+
+                for (let i = 1; i <= daysInMonth; i++) {
+                    // Converting to day to the week and testing to know if it's today
+                    let weekday = convertDayToWeekDay(i);
+                    let today = i == now.getDate() ? "todays-checkbox-container" : ""
+
+                    text += `
+                  <div class="checkbox-container ${today}">        
+                    <input 
+                    type="checkbox"
+                    data-day="${i}" 
+                    data-weekday="${weekday}" 
+                    data-assigned-day="${task.days}"
+                    <span class="checkbox"></span>
+                  </div>`
+                }
+            });
+        });
     });
-  });
 
-  document.getElementById("test_div").innerHTML = text;
+    document.getElementById("test_div").innerHTML = text;
 }
+// Making checkboxes bold
+function adaptCheckboxClass() {
 
-function adaptCheckboxClass(){
-  
     for (let checkbox of document.getElementsByTagName('input')) {
-      // for (let weekday of checkbox.dataset.weekday.split(','))
-        if(checkbox.dataset.assignedDay.includes(checkbox.dataset.weekday) ){
-          checkbox.classList.add('bold-checkbox')
-          
-        }
-        if(checkbox.dataset.assignedDay === checkbox.dataset.day){
-          checkbox.classList.add('bold-checkbox')
-          // if(now.getDate() === checkbox.dataset.day){
-          //   checkbox.classList.add('red')
-          // }
+        if (checkbox.dataset.assignedDay.includes(checkbox.dataset.weekday)) {
+            checkbox.classList.add('bold-checkbox')
 
         }
-        
+        if (checkbox.dataset.assignedDay === checkbox.dataset.day) {
+            checkbox.classList.add('bold-checkbox')
+        }
+
     }
 }
 
-function convertDayToWeekDay(i){
+function convertDayToWeekDay(i) {
     let currentDate = new Date(year, month, i);
     let weekday = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(currentDate);
 
